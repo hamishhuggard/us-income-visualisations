f869a1188f3aabbd284d6cfdd514c7399986dc7b
hamishhuggard <hamishhuggard@gmail.com>
2024-02-04 00:15:39 +1300

    Animate the plot

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,4 @@
 d3.json('data.json').then(function(data) {
-    console.log(data);
     const margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
@@ -18,50 +17,52 @@ d3.json('data.json').then(function(data) {
         .domain(data.yLimits)
         .range([height, 0]);
 
-    const xAxis = d3.axisBottom(x)
-        .tickValues(data.labelsX)
-        .tickFormat(function(d, i) { return data.labels[i]; });
-
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(d3.axisBottom(x).tickValues(data.labelsX).tickFormat((d, i) => data.labels[i]));
 
     svg.append("g")
         .attr("class", "y axis")
         .call(d3.axisLeft(y));
 
-    // Populate dropdown
-    const select = d3.select("#yearSelect")
-        .on("change", function() {
-            updatePlot(this.value);
-        });
+    let currentYearIndex = 0;
 
-    select.selectAll("option")
-        .data(data.years)
-      .enter().append("option")
-        .text(function(d) { return d; });
-
-    function updatePlot(year) {
-        const values = data.values[year];
-        const bars = svg.selectAll(".bar")
-            .data(values);
-
-        bars.enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d, i) { return x(data.binsX[i]); })
-            .attr("width", x(data.binsX[1]) - x(data.binsX[0]) - 1)
-          .merge(bars)
-            .transition()
-            .duration(1000)
-            .attr("y", function(d) { return y(d); })
-            .attr("height", function(d) { return height - y(d); });
-
-        bars.exit().remove();
-
-        d3.select("#plotTitle").text("Distribution for Year: " + year);
+    function animateYears() {
+        if (currentYearIndex < data.years.length) {
+            const year = data.years[currentYearIndex];
+            updatePlot(year);
+            // Calculate the delay for the current year
+            const delay = data.timePerYear[currentYearIndex] * 1000; // Convert seconds to milliseconds
+            currentYearIndex++;
+            setTimeout(animateYears, delay);
+        } else {
+            // Optionally, loop the animation
+            currentYearIndex = 0; // Reset to start
+            animateYears(); // Restart the animation
+        }
     }
 
-    // Initially load the plot for the first year
-    updatePlot(data.years[0]);
+function updatePlot(year) {
+    const values = data.values[year];
+    const update = svg.selectAll(".bar").data(values);
+    const enter = update.enter().append("rect").attr("class", "bar")
+                    .attr("x", (d, i) => x(data.binsX[i]))
+                    .attr("width", x(data.binsX[1]) - x(data.binsX[0]) - 1);
+    
+    // Updating existing bars
+    update.merge(enter)
+        .transition().duration(1000)
+        .attr("y", d => y(d)) // Set the new y position based on the data value
+        .attr("height", d => height - y(d)); // Adjust the height from the base to the new y position
+
+    // Removing any bars that no longer have corresponding data
+    update.exit().remove();
+
+    d3.select("#plotTitle").text("Distribution for Year: " + year);
+}
+
+    // Start the animation
+    animateYears();
 });
+
